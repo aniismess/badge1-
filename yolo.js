273a365e1745d8559bd374f0c0ae1b29d14c1a42
Animesh mishra <142673038+aniismess@users.noreply.github.com>
2024-03-22 23:17:34 +0530

    Create yolo.js

---
+++ yolo.js
@@ -0,0 +1,12 @@
+const form = document.createElement("form");
+const input = document.createElement("input");
+const button = document.createElement("button");
+
+input.type = "text";
+button.type = "submit";
+button.textContent = "Submit";
+
+form.appendChild(input);
+form.appendChild(button);
+
+document.body.appendChild(form);
